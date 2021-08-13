c4811f1ed50ddb6a918c4c0bcb8c0476caaa361b
Giorgi <giorgi.bitsadze01@gmail.com>
2021-08-13 18:30:09 +0400

    .

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,54 @@
+// hero
+
+$(document).ready(function() {
+    var $magic = $(".magic"),
+        magicWHalf = $magic.width() / 2;
+    $(document).on("mousemove", function(e) {
+      $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+    });
+  });
+
+// section 4
+
+const methods = document.querySelectorAll('.section-4 .content table tr td .method .choose p');
+
+const paypals = document.querySelectorAll('.section-4 .content table tr td .paypal')
+
+const cryptos = document.querySelectorAll('.section-4 .content table tr td .crypto')
+
+methods[0].addEventListener('click', () => {
+    enableActive(methods[0]);
+    cryptos.forEach(item => {
+        if(item.classList.contains('show')){
+            item.classList.remove('show');
+        }
+    })
+    paypals.forEach(item => {
+        item.classList.add('show');
+    })
+})
+
+methods[1].addEventListener('click', () => {
+    enableActive(methods[1]);
+    paypals.forEach(item => {
+        if(item.classList.contains('show')){
+            item.classList.remove('show');
+        }
+    })
+    cryptos.forEach(item => {
+        item.classList.add('show');
+    })
+})
+
+function enableActive(target){
+    methods.forEach(method => {
+        method.classList.remove('active');
+    })
+    target.classList.add('active')
+}
+
+// section 5
+
 
 
 // dropdowns
